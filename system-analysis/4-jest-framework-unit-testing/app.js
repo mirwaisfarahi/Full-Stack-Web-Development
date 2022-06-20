41709d01f2ac5c18f0601ff1abb20b8cd6530179
Mirwais Farahi <waisfarahi@gmail.com>
2022-06-20 22:28:48 +0500

    app.js

---
+++ system-analysis/4-jest-framework-unit-testing/app.js
@@ -0,0 +1,20 @@
+const {fetchData} = require('./http.js');
+
+const button = document.querySelector('button');
+
+const loadTitle = () => {
+  return fetchData().then(extractedData => {
+    const title = extractedData.title;
+    const transformedTitle = title.toUpperCase();
+    return transformedTitle;
+  });
+};
+
+const printTitle = () => {
+  loadTitle().then(title => {
+    console.log(title);
+  });
+};
+
+button.addEventListener('click', printTitle);
+exports.printTitle = printTitle;
