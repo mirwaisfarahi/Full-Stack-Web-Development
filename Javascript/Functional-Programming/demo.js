37ac3d01aec052f667d80a0d07c8a653ff981bb5
= <mirfarahi92@gmail.com>
2023-11-04 15:26:44 +0430

    demo edited

--- Javascript/Functional-Programming/demo.js
+++ Javascript/Functional-Programming/demo.js
@@ -1,27 +1,20 @@
-const uncommonFromSentences = (s1 = "this apple is sweet", s2 = "this apple is sour") => {
-    
-    // use hash table => frequency counter and object/set
-    const wordCounter = {};
+const longestConsecutive = function(nums = [9,1,4,7,3,-1,0,5,8,-1,6]) {    
+    if (nums.length == 0) return [];
+    const setValues = new Set(nums.sort((a, b) => a - b));
+    const sequenceCounter = [];
+    let counter = 1;
 
-    // add array to store the result
-    const result = [];
-
-    const arr1 = s1.split(' ');
-    const arr2 = s2.split(' ');
-
-    for(let val of arr1) {
-        wordCounter[val] = (wordCounter[val] || 0) + 1;
+    for (let i = 0; i < setValues.length; i++) {
+        if (setValues[i] == (setValues[i - 1] + 1)) {
+            counter = counter + 1;
+            sequenceCounter.push(counter);
+        }
+        else {
+            counter = 1;
+        }
     }
 
-    for(let val of arr2) {
-        wordCounter[val] = (wordCounter[val] || 0) + 1;
-    }
+    return sequenceCounter;
+};
 
-    for(key in wordCounter) {
-        if(wordCounter[key] == 1) result.push(key)
-    }
-
-    return result;
-}
-
-console.log(uncommonFromSentences());
+console.log(longestConsecutive());
