e2cd1e22c766fde482cd0a1e2ab0e4e201941482
Mirwais Farahi <waisfarahi@gmail.com>
2023-11-04 22:57:19 +0500

    edit

--- Javascript/Functional-Programming/demo.js
+++ Javascript/Functional-Programming/demo.js
@@ -1,20 +1,27 @@
-const longestConsecutive = function(nums = [9,1,4,7,3,-1,0,5,8,-1,6]) {    
+const longestConsecutive = function(nums = [-6,-1,-1,9,-8,-6,-6,4,4,-3,-8,-1]) {    
     if (nums.length == 0) return [];
     const setValues = new Set(nums.sort((a, b) => a - b));
-    const sequenceCounter = [];
-    let counter = 1;
+    const arrayFromSet = Array.from(setValues);
 
-    for (let i = 0; i < setValues.length; i++) {
-        if (setValues[i] == (setValues[i - 1] + 1)) {
+    let counter = 1;
+    let maxCounter = 1;
+    for (let i = 0; i < arrayFromSet.length; i++) {
+        if (arrayFromSet[i] == (arrayFromSet[i - 1] + 1)) {
             counter = counter + 1;
-            sequenceCounter.push(counter);
+            maxCounter = Math.max(maxCounter, counter)
         }
         else {
             counter = 1;
         }
     }
 
-    return sequenceCounter;
+    return maxCounter;
 };
 
 console.log(longestConsecutive());
+
+
+[7,2 ,1, 3, 6, 9, 12, 13, 15, 14, 14] // set
+
+[1, 2, 3, 6, 7, 9, 12, 13, 14, 15]
+
